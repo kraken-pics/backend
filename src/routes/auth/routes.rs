7340755092f51bc8f4a8653f05dfdffab7a11f61
ian <ianfogy21@gmail.com>
2022-07-03 17:49:17 +0100

    feat(auth): register (unfinished)

--- src/routes/auth/routes.rs
+++ src/routes/auth/routes.rs
@@ -1,6 +1,6 @@
 use crate::{entity::user, state::state::AppState, util::response::ApiResponse};
 use actix_web::{post, web, Error, Responder, Result, Scope};
-use sea_orm::{ColumnTrait, EntityTrait, QueryFilter};
+use sea_orm::{ColumnTrait, Condition, EntityTrait, QueryFilter};
 use serde::Deserialize;
 
 // global AppState
@@ -10,6 +10,14 @@ type AppData = web::Data<AppState>;
 pub struct ILogin {
     pub username: String,
     pub password: String,
+}
+
+#[derive(Deserialize)]
+pub struct IRegister {
+    pub username: String,
+    pub email: String,
+    pub password: String,
+    pub recaptcha: String,
 }
 
 // export auth's routes
@@ -47,4 +55,77 @@ async fn login(data: web::Json<ILogin>, state: AppData) -> Result<impl Responder
         success: false,
         message: found_user.username.to_string(),
     }));
+}
+
+#[post("/register")]
+async fn register(data: web::Json<IRegister>, state: AppData) -> Result<impl Responder, Error> {
+    match recaptcha::verify(
+        dotenv::var("RECAPTCHA_SECRET")
+            .expect("RECAPTCHA_SECRET not set in env")
+            .as_str(),
+        &data.recaptcha,
+        None,
+    )
+    .await
+    {
+        Ok(_) => (),
+        Err(err) => {
+            return Ok(actix_web::web::Json(ApiResponse {
+                success: false,
+                message: err.to_string(),
+            }))
+        }
+    };
+
+    if data.username.len() < 3 {
+        return Ok(actix_web::web::Json(ApiResponse {
+            success: false,
+            message: "Username too short".to_string(),
+        }));
+    }
+
+    if data.username.len() > 30 {
+        return Ok(actix_web::web::Json(ApiResponse {
+            success: false,
+            message: "Username too long".to_string(),
+        }));
+    }
+
+    if data.password.len() < 5 {
+        return Ok(actix_web::web::Json(ApiResponse {
+            success: false,
+            message: "Username too short".to_string(),
+        }));
+    }
+
+    if user::Entity::find()
+        .filter(
+            Condition::any()
+                .add(user::Column::Username.eq(data.username.to_owned()))
+                .add(user::Column::Email.eq(data.email.to_owned())),
+        )
+        .one(&state.db)
+        .await
+        .expect("User not found")
+        .is_some()
+    {
+        return Ok(actix_web::web::Json(ApiResponse {
+            success: false,
+            message: "Username/email already used".to_string(),
+        }));
+    };
+
+    user::Entity::insert(user::ActiveModel {
+        username: todo!(""),
+        email: todo!(""),
+        password: todo!(""),
+        token: todo!(""),
+        uploadtoken: todo!(""),
+        ..Default::default()
+    });
+
+    Ok(actix_web::web::Json(ApiResponse {
+        success: false,
+        message: "meow".to_string(),
+    }))
 }

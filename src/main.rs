aa5b7c51ba867b49510ef390b5146db1a2013d19
eeyin <ianfogy21@gmail.com>
2022-07-31 01:57:06 +0100

    feat: guard routes for beta server release

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
 use actix_identity::{CookieIdentityPolicy, IdentityService};
-use actix_web::HttpServer;
+use actix_web::guard::GuardContext;
+use actix_web::{guard, HttpServer};
 use actix_web::{web, App};
 use dotenv::dotenv;
 
 use kraken::controllers;
 use kraken::state;
 
+fn protect_routes(req: &GuardContext) -> bool {
+    let guard_secret = dotenv::var("GUARDED_SECRET").expect("GUARDED_SECRET envar");
+    let guarded_val = match req.clone().head().headers.get("x-guarded") {
+        Some(val) => val,
+        None => return false,
+    };
+
+    if guarded_val.to_str().unwrap() != guard_secret {
+        return false;
+    }
+
+    true
+}
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
@@ -29,7 +43,7 @@ async fn main() -> std::io::Result<()> {
             // state middleware
             .app_data(web::Data::new(app_state.to_owned()))
             .app_data(web::Data::new(state::AppState::init_multipart()))
-            .service(controllers::routes::get())
+            .service(controllers::routes::get().guard(guard::fn_guard(protect_routes)))
     })
     // bind to localhost on envar port
     .bind(("127.0.0.1", port.parse().clone().unwrap()))?
